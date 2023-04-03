80939650452c650b2820c0564931619a0fc55ffb
Michael Lee <mikelee0330@gmail.com>
2023-04-02 22:43:28 -0700

    fix image index

--- client/src/Components/AnimalCard.js
+++ client/src/Components/AnimalCard.js
@@ -16,7 +16,9 @@ function AnimalCard({ animal, className, fetchAnimal, setSearchQuery, characteri
             }
           });
           const pexelData = await pexelsData.json()
-          setImageUrl(pexelData.photos[Math.floor(Math.random()*10)]?.src.medium)
+          let pexelInd = pexelData.photos.length > 9 ? Math.floor(Math.random()*10) : 0
+          setImageUrl(pexelData.photos[pexelInd]?.src.medium)
+          console.log(imageUrl)
       };
 
       const name = animal[animal.length - 1] === "s" ? animal.substring(0, animal.length - 1) : animal
